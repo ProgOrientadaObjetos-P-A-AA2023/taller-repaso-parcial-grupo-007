2f655895456f2fc3b69df8ef9a5822e3ad03b91b
Denis Ruiz <denisruiz112789@gmail.com>
2023-07-05 12:04:26 -0500

    Tallergrupo007

--- EjercicioRepaso/src/paquete002/Ciudad.java
+++ EjercicioRepaso/src/paquete002/Ciudad.java
@@ -9,5 +9,25 @@ package paquete002;
  * @author reroes
  */
 public class Ciudad {
+
     String nombreCiudad;
+
+    public Ciudad(String n) {
+        nombreCiudad = n;
+    }
+
+    public String obtenerNombreCiudad() {
+        return nombreCiudad;
+    }
+
+    public void establecerNombreCiudad(String n) {
+        nombreCiudad = n;
+    }
+
+    @Override
+    public String toString() {
+        String m = String.format("Nombre Ciudad: %s/n",
+                obtenerNombreCiudad());
+    return m;
+    }
 }
--- EjercicioRepaso/src/paquete002/Propiedad.java
+++ EjercicioRepaso/src/paquete002/Propiedad.java
@@ -10,4 +10,22 @@ package paquete002;
  */
 public class Propiedad {
     public double costoPropiedad;
+
+    public Propiedad(double costoPro) {
+        costoPropiedad = costoPro;
+    }
+
+    public double obtenerCostoPropiedad() {
+        return costoPropiedad;
+    }
+
+    public void establecerCostoPropiedad(double c) {
+        costoPropiedad = c;
+    }
+    @Override
+    public String toString() {
+        String m = String.format("Costo Propiedad: %.2f/n",
+                obtenerCostoPropiedad());
+    return m;
+    }
 }
